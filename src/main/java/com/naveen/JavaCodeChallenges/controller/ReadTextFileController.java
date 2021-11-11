30bb49b5e91008a2b201b935e61c56cd1a0dd32e
Naveen Reddy <naveenrg914@gmail.com>
2021-11-11 16:37:56 -0600

    added the core login to the service class to read the input text file from resources folder

--- src/main/java/com/naveen/JavaCodeChallenges/controller/ReadTextFileController.java
+++ src/main/java/com/naveen/JavaCodeChallenges/controller/ReadTextFileController.java
@@ -2,17 +2,23 @@ package com.naveen.JavaCodeChallenges.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.naveen.JavaCodeChallenges.service.ReadTextFileService;
 
+/*
+ * API to read a text file 
+ */
+
 @RestController
+@RequestMapping("/read")
 public class ReadTextFileController {
 
 	@Autowired
 	ReadTextFileService readTextFileService;
 
-	@GetMapping("/read")
+	@GetMapping()
 	public StringBuilder readFile() {
 		return readTextFileService.readFile();
 	}
--- src/main/java/com/naveen/JavaCodeChallenges/service/ReadTextFileService.java
+++ src/main/java/com/naveen/JavaCodeChallenges/service/ReadTextFileService.java
@@ -1,12 +1,35 @@
 package com.naveen.JavaCodeChallenges.service;
 
+import java.io.FileReader;
+import java.util.Scanner;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReadTextFileService {
 
 	public StringBuilder readFile() {
-		return null;
+
+		StringBuilder stringBuilder = new StringBuilder();
+		Scanner scanner;
+
+		try {
+
+			scanner = new Scanner(new FileReader("src/main/resources/input.txt"));
+
+			while (scanner.hasNextLine()) {
+
+				stringBuilder.append(scanner.nextLine());
+			}
+
+			scanner.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(stringBuilder.toString());
+
+		return stringBuilder;
 	}
 
 }
