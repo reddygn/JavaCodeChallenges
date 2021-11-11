5a3400ee79541235177b5525cce56db0f5ae4103
Naveen Reddy <naveenrg914@gmail.com>
2021-11-11 16:13:23 -0600

    first commit

---
+++ src/main/java/com/naveen/JavaCodeChallenges/JavaCodeChallengesApplication.java
@@ -0,0 +1,13 @@
+package com.naveen.JavaCodeChallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeChallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naveen/JavaCodeChallenges/JavaCodeChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.JavaCodeChallenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
